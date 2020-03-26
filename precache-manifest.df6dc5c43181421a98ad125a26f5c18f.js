529024cd8e3cdf5692d293d4869697ed885c0610
tiffhuddleston <tiffhuddleston@gmail.com>
2020-03-26 18:00:30 -0500

    Updates

---
+++ precache-manifest.df6dc5c43181421a98ad125a26f5c18f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d472847a32c31c8749b69234ed805be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf55c0769eb388772e50",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "661e4924caf3d4cf4de9",
+    "url": "/static/css/main.2d8f81f7.chunk.css"
+  },
+  {
+    "revision": "cf55c0769eb388772e50",
+    "url": "/static/js/2.d69e8191.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.d69e8191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661e4924caf3d4cf4de9",
+    "url": "/static/js/main.61e4d4cc.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  },
+  {
+    "revision": "f4b16b4be1c18d5f6c977ab19577c397",
+    "url": "/static/media/GatherUp.f4b16b4b.png"
+  },
+  {
+    "revision": "415dc8e68166e37f4e899f3ea7240da8",
+    "url": "/static/media/GroupLoop1.415dc8e6.png"
+  },
+  {
+    "revision": "a96c37ea077e7ac54ac10fd459bb9610",
+    "url": "/static/media/RickandMorty.a96c37ea.png"
+  },
+  {
+    "revision": "c592f214bba78cd59ab6920f650b6ba3",
+    "url": "/static/media/nerd-trivia.c592f214.png"
+  }
+]);
